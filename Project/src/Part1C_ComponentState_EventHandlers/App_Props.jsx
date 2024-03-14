f3e47120e9ce506844f22888dfeb7e632066e440
Zhi Xin <limzhixin.lzx@gmail.com>
2024-03-14 23:04:05 +0800

    Part1c - Pass in props from parent to child component

---
+++ Project/src/Part1C_ComponentState_EventHandlers/App_Props.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import '../App.css';
+
+// 5 - When value of any parent's props change, re-rendering of React component is not triggered automatically to reflect display on new value
+// |_ refer to main.jsx -> need to call .render() manually
+const App = ({ counter }) => {
+  const name = 'Peter';
+  const age = 10;
+  // alert(`Counter passed in by Parent: ${counter}`);
+  return (
+    <div>
+      <h1>Greetings</h1>
+      {/* 3 - Individual props is bundled in parent component as JSON object && passed to child Component*/}
+      <Component1_PassInProps name="Maya" age={26 + 10} />
+      <Component1_PassInProps name={name} age={age} />
+      <Component2_DestructurePropObj name="Simon" age={12} />
+      <Component3_DestructurePropParam name="Andrew" age={24} />
+      <Component4_ResponsiveCounter counter={counter} />
+    </div>
+  );
+};
+
+const Component1_PassInProps = (props) => {
+  // 2 - When function is defined inside a component, it has access to component's props
+  //     |_ no need pass in as function param
+  function bornYear() {
+    return ((new Date().getFullYear()) - props.age);
+  }
+
+  return (
+    <div>
+      <p>Component1_PassInProps {props.name}, you are {props.age} years old</p>
+      {/* 1 - Pass in function for rendering component */}
+      <p>So you were probably born in {bornYear()}</p>
+    </div >
+  );
+};
+
+const Component2_DestructurePropObj = (items) => {
+  let { name, age, random } = items; // 3 - Parent's props are wrapped & passed in JSON object, destructured base on key name
+  return (
+    <>
+      <h3>Destructured</h3>
+      <p>Your name is {name}, age {age}. Note: {random}</p>
+    </>
+  );
+};
+
+// 4 - Destructure parent's prop bundled as JSON object in function argument
+const Component3_DestructurePropParam = ({ name, age }) => {
+  return (
+    <>
+      <h3>Destructured as param</h3>
+      <p>Your name is {name}, age {age}.</p>
+    </>
+  );
+};
+
+const Component4_ResponsiveCounter = (props) => {
+  const { counter } = props;
+  return (
+    <button>{counter}</button>
+  );
+};
+
+export default App;
--- Project/src/main.jsx
+++ Project/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './Part1A_IntroToReact/App.jsx';
+import App2 from './Part1C_ComponentState_EventHandlers/App_Props.jsx';
 import './index.css';
 
 // In index.html, retrieve element with ID 'root' & render React component as child of retrieved element
@@ -17,10 +18,23 @@ ReactDOM.createRoot(document.getElementById('part1b')).render(
   </React.StrictMode>,
 );
 
+// 5 - .render() need to be called for re-rendering React component manually whenever variable changes to reflect on display
+let counter = 1;
 ReactDOM.createRoot(document.getElementById('part1c')).render(
   <React.StrictMode>
-    <App />
+    <App2 counter={counter} />
   </React.StrictMode>,
 );
+counter = 2;
 
+
+// 5 - Another way is to trigger re-render every x seconds to reflect changes
+setInterval(() => {
+  ReactDOM.createRoot(document.getElementById('part1c_2')).render(
+    <React.StrictMode>
+      <App2 counter={counter} />
+    </React.StrictMode>,
+  );
+  counter += 1;
+}, 1000)
 
