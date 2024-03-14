43d6fc520645bb27e8b909adbc2535a8f89f0024
Zhi Xin <limzhixin.lzx@gmail.com>
2024-03-14 19:46:14 +0800

    Restructure

--- Project/src/main.jsx
+++ Project/src/main.jsx
@@ -4,15 +4,23 @@ import App from './Part1A_IntroToReact/App.jsx';
 import './index.css';
 
 // In index.html, retrieve element with ID 'root' & render React component as child of retrieved element
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('title')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
 
 // Will override any content in accessed element with React Components
-ReactDOM.createRoot(document.getElementById('title')).render(
+ReactDOM.createRoot(document.getElementById('part1b')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
+ReactDOM.createRoot(document.getElementById('part1c')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
+
+
